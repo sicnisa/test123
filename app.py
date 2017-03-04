6b29f24c4f9b831c08311a54cbd9358b641b07b8
Hx <Hx>
2017-03-04 13:31:13 +0800

    a2

---
+++ app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+'''
+main module
+Copyright (c) 2017 ISA
+'''
+import sys
+
+from script.penfuncs import main
+
+if __name__ == '__main__':
+    main()
+    
---
+++ script/penfuncs.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import sys
+
+
+def main():
+    reload(sys)
+    sys.setdefaultencoding("utf8")
